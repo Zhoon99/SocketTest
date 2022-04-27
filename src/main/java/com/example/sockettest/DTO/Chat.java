dbeae4271df07bb4a3b4f8de670b25231654075b
Zhoon99 <chjh827@gmail.com>
2022-04-28 00:40:54 +0900

    STOMP 테스트

---
+++ src/main/java/com/example/sockettest/DTO/Chat.java
@@ -0,0 +1,26 @@
+package com.example.sockettest.DTO;
+
+public class Chat {
+
+	private String name;
+	private String content;
+	
+	public Chat(String name, String content) {
+		this.name = name;
+		this.content = content;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
